ebdd42af2991f6601801f9f4a61fa0b11a3eff35
Thomas Tumiel <ttumiel@gmail.com>
2018-09-30 20:05:58 +0200

    change to list since order is important

--- bot/telegram/parse_hn.py
+++ bot/telegram/parse_hn.py
@@ -3,11 +3,8 @@ import requests
 
 def parse_hn():
     """Parse top 5 results from Hacker News"""
-
     hn = requests.get("https://news.ycombinator.com/").text
     soup = BeautifulSoup(hn, 'html.parser')
     new_posts = soup.find_all("a", class_="storylink")
 
-    return {new_posts[i].get_text(): new_posts[i].get("href") for i in range(5)}
-
-
+    return [(new_posts[i].get_text(), new_posts[i].get("href")) for i in range(5)]
