74902bb024586964c61b7a0c3ffcc76e89c1ae73
Thomas Tumiel <ttumiel@gmail.com>
2018-09-30 21:28:09 +0200

    fix urls

--- bot/telegram/urls.py
+++ bot/telegram/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from views import CommandReceiveView
+from .views import CommandReceiveView
 
+app_name = 'telegram'
 urlpatterns = [
     url(r'^bot/(?P<bot_token>.+)/$', CommandReceiveView.as_view(), name='command'),
 ]
