ea422b6ade0ea0b4cc721993827f6033cbd2d251
Thomas Tumiel <ttumiel@gmail.com>
2018-09-30 22:41:48 +0200

    generalise hn parsing

--- bot/telegram/utils.py
+++ bot/telegram/utils.py
@@ -1,10 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 
-def parse_hn():
+def parse_hn(number=5, site=""):
     """Parse top 5 results from Hacker News"""
-    hn = requests.get("https://news.ycombinator.com/").text
+    number=5 if number>30 or number<1
+    hn = requests.get("https://news.ycombinator.com/" + site).text
     soup = BeautifulSoup(hn, 'html.parser')
     new_posts = soup.find_all("a", class_="storylink")
 
-    return [(new_posts[i].get_text(), new_posts[i].get("href")) for i in range(5)]
+    return [(new_posts[i].get_text(), new_posts[i].get("href")) for i in range(number)]
+
+
