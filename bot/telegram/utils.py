38fcdaf1c4f9ca308d1d31e26601eb8dc90e346e
Thomas Tumiel <ttumiel@gmail.com>
2018-10-16 15:54:45 +0200

    add firebase method for parsing data

--- bot/telegram/utils.py
+++ bot/telegram/utils.py
@@ -1,16 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
-from firebase_admin import db
+from firebase import firebase
 
-def parse_hn(number=5, site=""):
+OPTIONS=["top", "ask", "show", "job", "best", "new"]
+
+OPTIONS_URL={"top":"", "ask":"ask", "show":"show", "job":"jobs", "best":"best", "new":"newest"}
+
+def parse_hn(option, number):
     """
     Parse top 5 results from Hacker News
     Use firebase method instead
     """
     if number>30 or number<1: number=5
-    hn = requests.get("https://news.ycombinator.com/" + site).text
+    hn = requests.get("https://news.ycombinator.com/" + OPTIONS_URL[option]).text
     soup = BeautifulSoup(hn, 'html.parser')
     new_posts = soup.find_all("a", class_="storylink")
+
+    print(len(new_posts))
 
     return [(new_posts[i].get_text(), new_posts[i].get("href")) for i in range(number)]
 
@@ -20,15 +26,20 @@ def check_string_num(num):
     except ValueError:
         return None
 
-def parse_hn_firebase():
+def parse_hn_firebase(option="top", number=5):
     """
     Parse Hacker News using the firebase API
     """
+    fb = firebase.FirebaseApplication('https://hacker-news.firebaseio.com', None)
 
-    # Get a database reference to our posts
-    ref = db.reference('/v0/topstories.json', url='https://hacker-news.firebaseio.com')
+    if option in OPTIONS:
+        post_ids = fb.get('/v0/' + option + 'stories', None)
+    else:
+        post_ids = fb.get('/v0/topstories', None)
 
-    # Read the data at the posts reference (this is a blocking operation)
-    print(ref.get())
+    results = []
+    for i in range(number):
+        data = fb.get('/v0/item/' + str(post_ids[i]), None)
+        results.append((data["title"], data["url"]))
 
-
+    return results
